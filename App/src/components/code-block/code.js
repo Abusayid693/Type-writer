7bb1f25fc3fb0331dd3db9b87c1b1f323143f8a5
Rehane <abusayid693@gmail.com>
2021-07-23 03:07:12 +0530

    Syntax high-lighter typo

--- App/src/components/code-block/code.js
+++ App/src/components/code-block/code.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { prism } from "react-syntax-highlighter/dist/esm/styles/hljs/docco";
 
+/** CodeView block is replaced with current div adding syntax highlighter to the current code   */
 function CodeView(props) {
   return (
     <div className="codes">
       <SyntaxHighlighter
         contentEditable="false"
         language="javascript"
-        style={docco}
+        style={prism}
+        wrapLines={true}
+        showLineNumbers={true}
       >
         {props.text}
       </SyntaxHighlighter>
@@ -17,42 +20,27 @@ function CodeView(props) {
   );
 }
 
+function codesyn(text, elem) {
+  ReactDOM.render(<CodeView text={text} />, document.querySelector(elem));
+}
 
-var numberOfCodeBlocks=0;
+// Variable to keep a track record of number of code-blocks
+var numberOfCodeBlocks = 0;
 
 function code(elem) {
   let newElement = document.createElement("div"),
     parent = elem.parentNode,
-    classIdentification="code-view"+numberOfCodeBlocks;
-  
-  newElement.classList.add("code-view",classIdentification);
-  // newElement.classList.add();
+    classIdentification = "code-view" + numberOfCodeBlocks;
 
+  newElement.classList.add("code-view", classIdentification);
   newElement.contentEditable = "true";
-
-  // newElement.innerHTML ="<SyntaxHighlighter contentEditable = 'true' language='javascript' style={docco}> function hello(){} </SyntaxHighlighter>"
-
+  newElement.focus();
   newElement.innerHTML =
-    "<div class='codes' ><i id='codeCopy' contentEditable='false' class='far copy fa-clipboard'></i><p contentEditable='true' class='code'>function () {}</p></div>";
+    "<div class='codes' contentEditable='true' ><i id='codeCopy' contentEditable='false' class='far copy fa-clipboard'></i><p contentEditable='true' class='code'>.</p></div>";
 
   parent.replaceChild(newElement, elem);
-  // ReactDOM.render(
-  //   <CodeView text="function()" />
-  // ,
-  // document.querySelector(".code-view")
-  // );
-  newElement.focus();
-  // copyCode();
   numberOfCodeBlocks++;
 }
-
-function codesyn(text, elem) {
-  ReactDOM.render(
-    <CodeView text={text} />,
-    document.querySelector(".code-view")
-  );
-}
-
 
 function copyCode() {
   var btn = document.querySelector("#codeCopy"),
@@ -68,8 +56,7 @@ function initialize() {
   document.addEventListener("keypress", (event) => {
     var elem = document.activeElement;
     if (event.ctrlKey && elem.classList.contains("code-view")) {
-      codesyn(elem.textContent, "."+elem.classList);
-      alert(elem.classList)
+      codesyn(elem.textContent, "." + elem.classList[1]);
       elem.classList.add("main");
     } else {
       console.log("Active element is not code block");
