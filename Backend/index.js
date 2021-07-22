d49912967751ae9cb556d2c1f08193b7d74a2841
Rehane <abusayid693@gmail.com>
2021-07-22 22:00:05 +0530

    Database connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,58 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const path = require('path');
-
-mongoose.connect("mongodb+srv://Rehan:Alexa693@cluster0.2wzmd.mongodb.net/typeWriter?retryWrites=true&writeConcern=majority/locationDB ",{ useUnifiedTopology: true }, { useNewUrlParser: true });
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const path = require("path");
 
 
-
-const app=express();
+const app = express();
 const PORT = process.env.PORT || 8000;
+app.use(morgan("tiny"));
 
-app.use(morgan('tiny'));
+mongoose.connect(
+  "mongodb+srv://Rehan:Alexa693@cluster0.2wzmd.mongodb.net/typeWriter?retryWrites=true&writeConcern=majority/locationDB ",
+  { useUnifiedTopology: true },
+  { useNewUrlParser: true }
+);
 
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
+// Schema
+const Schema = mongoose.Schema;
+const TextFileSchema = new Schema({
+  title: String,
+  body: String,
+  date: {
+    type: String,
+    default: Date.now(),
+  }
+});
+
+// Model
+
+const TextFile = mongoose.model("TextFile", TextFileSchema);
+
+// Test data
+
+const test = {
+  title: "About over hype of Machine learning",
+  body: "<h1>Hello</h1>",
+};
+
+const newTextFile = new TextFile(test);
+newTextFile.save((error)=>{
+    if(error)
+    console.log(error);
+    else
+    console.log("Text file added to database");
+});
 
 
-mongoose.connection.on('connected',()=>{
-    console.log("Database successfully connected")
-})
 
-app.listen(PORT,console.log("Server running on port"))
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Database successfully connected");
+});
+
+app.listen(PORT, console.log("Server running on port"));
