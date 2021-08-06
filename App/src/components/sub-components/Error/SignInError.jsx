5f99100c54445361b86f392252a5739619d94a9a
Rehane <abusayid693@gmail.com>
2021-08-06 23:50:59 +0530

    Loader and Error alert added

---
+++ App/src/components/sub-components/Error/SignInError.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Alert, AlertTitle } from '@material-ui/lab';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function SignInError(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+       <Alert variant="outlined" severity="error">{props.text}</Alert>
+    </div>
+  );
+}
