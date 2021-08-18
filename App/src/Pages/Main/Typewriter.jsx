584dd2e190cc57c408ccebbb1414a966d8ba5b42
Rehan <Abusayid693@gmail.com>
2021-08-18 21:18:20 +0530

    Update Typewriter.jsx

--- App/src/Pages/Main/Typewriter.jsx
+++ App/src/Pages/Main/Typewriter.jsx
@@ -31,15 +31,12 @@ class Typewriter extends React.Component {
     this.typewriter();
     continueWithCodingBlocks();
   }
-
   typewriter() {
     var i = 0,
       b = 0,
       initial_release = 0;
-
     /** EVENT Append : When user presses `Enter` it creates a new `p`
         element and append it to main child body, only to replace the child element according to users entry **/
-
     document.addEventListener('keypress', append);
 
     function append(e) {
@@ -48,16 +45,15 @@ class Typewriter extends React.Component {
           parent,
           tempElement = document.createElement('hr'),
           appendingElem = document.createElement('p');
+        
         appendingElem.classList.add('main');
-
         // ERROR HANDLING
         if (!initial_release) elem = document.querySelector('.pad');
         else elem = document.activeElement;
-
+        
         parent = elem.parentNode;
         appendingElem.contentEditable = 'true';
         initial_release = 1;
-
         // EXCEPTION HANDLING : for Li elements
         if (elem.nodeName == 'LI') {
           elem = elem.parentNode;
@@ -68,10 +64,8 @@ class Typewriter extends React.Component {
           elem.parentNode.insertBefore(tempElement, elem.nextSibling);
           tempElement.parentNode.insertBefore(appendingElem, tempElement.nextSibling);
         }
-
         // EVENT HANDLING : Formatting equations
         if (document.activeElement.textContent[0] == '$') formatEquation(document.activeElement);
-
         appendingElem.focus();
         e.preventDefault();
         i = 0;
@@ -79,9 +73,7 @@ class Typewriter extends React.Component {
         return 1;
       }
     }
-
     // EVENT REPLACE = The primary function for replacing elements as per user prefix entries [ eg : prefix "#" for h1  ] **/
-
     document.addEventListener('keypress', replace);
     function replace(e) {
       if (e.keyCode == 32) {
@@ -90,7 +82,6 @@ class Typewriter extends React.Component {
 
         // EVENT HANDLING : for ordered lists
         if (!isNaN(text) && text !== '') ordered(elem, text);
-
         // MAIN EVENT HANDALING
         switch (text) {
           case '#':
